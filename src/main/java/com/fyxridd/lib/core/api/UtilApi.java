465c433003b37c7a866642158554753ba3725edd
yuexiangfeng <fyxridd@163.com>
2016-05-12 17:20:55 +0800

    更新

--- src/main/java/com/fyxridd/lib/core/api/UtilApi.java
+++ src/main/java/com/fyxridd/lib/core/api/UtilApi.java
@@ -26,6 +26,39 @@ public class UtilApi {
 
     private static Pattern ColorPattern = Pattern.compile("&[0123456789abcdeflmnor]");
 
+    private static final Map<Class<?>, Class<?>> WRAPPER_TO_PRIMITIVE_TYPE;
+    private static final Map<Class<?>, Class<?>> PRIMITIVE_TO_WRAPPER_TYPE;
+    static {
+        Map<Class<?>, Class<?>> primToWrap = new HashMap<Class<?>, Class<?>>(16);
+        Map<Class<?>, Class<?>> wrapToPrim = new HashMap<Class<?>, Class<?>>(16);
+
+        add(primToWrap, wrapToPrim, boolean.class, Boolean.class);
+        add(primToWrap, wrapToPrim, byte.class, Byte.class);
+        add(primToWrap, wrapToPrim, char.class, Character.class);
+        add(primToWrap, wrapToPrim, double.class, Double.class);
+        add(primToWrap, wrapToPrim, float.class, Float.class);
+        add(primToWrap, wrapToPrim, int.class, Integer.class);
+        add(primToWrap, wrapToPrim, long.class, Long.class);
+        add(primToWrap, wrapToPrim, short.class, Short.class);
+        add(primToWrap, wrapToPrim, void.class, Void.class);
+
+        WRAPPER_TO_PRIMITIVE_TYPE = Collections.unmodifiableMap(wrapToPrim);
+        PRIMITIVE_TO_WRAPPER_TYPE = Collections.unmodifiableMap(primToWrap);
+    }
+
+    public static <T> Class<T> wrap(Class<T> type) {
+        @SuppressWarnings("unchecked")
+        Class<T> wrapped = (Class<T>) PRIMITIVE_TO_WRAPPER_TYPE.get(type);
+        return (wrapped == null) ? type : wrapped;
+    }
+
+    public static <T> Class<T> unwrap(Class<T> type) {
+        // cast is safe: long.class and Long.class are both of type Class<Long>
+        @SuppressWarnings("unchecked")
+        Class<T> unwrapped = (Class<T>) WRAPPER_TO_PRIMITIVE_TYPE.get(type);
+        return (unwrapped == null) ? type : unwrapped;
+    }
+    
     /**
      * 获取带后缀的文件名
      * @param fullName 全名,如"a/b/name.hbm.xml"
@@ -669,6 +702,15 @@ public class UtilApi {
      */
     public static String md5(String msg) {
         return MD5.GetMD5Code(msg);
+    }
+
+    private static void add(
+            Map<Class<?>, Class<?>> forward,
+            Map<Class<?>, Class<?>> backward,
+            Class<?> key,
+            Class<?> value) {
+        forward.put(key, value);
+        backward.put(value, key);
     }
 
     private static FancyMessage get(int id, Object... args) {
